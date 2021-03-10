386bdebf55b2d026bc05e945a5dc2c3eedb878b6
risika <64900522+0722286293@users.noreply.github.com>
2021-03-11 02:45:47 +0300

    first commit

---
+++ area.py
@@ -0,0 +1,17 @@
+def area(a,b):
+  rectangle=(a*b)
+  print(rectangle)
+area(13,25)
+area(4,7)
+def area(pi,r):
+  circle=(pi*r*r)
+  print(circle)
+area(3.147,6)
+def volume(pi,r,h):
+  cylinder=(pi*r*r*h)
+  print(cylinder)
+volume(3.147,10,30)
+def volume(pi,r):
+  sphere=(1.33*pi*r*r*r)
+  print(sphere)
+volume(43,32)
